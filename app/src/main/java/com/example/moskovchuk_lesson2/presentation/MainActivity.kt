9709900c96b6e97b8abfab8f9a25ce1fe426cd71
Alena Moskovchuk <am79819712119@gmail.com>
2023-02-11 20:39:22 +0300

    add second screen

--- app/src/main/java/com/example/moskovchuk_lesson2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moskovchuk_lesson2/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moskovchuk_lesson2
+package com.example.moskovchuk_lesson2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moskovchuk_lesson2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
