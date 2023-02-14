66b5d6409e48ab57a8be344c3ac2c2a712cf8fef
Alena Moskovchuk <am79819712119@gmail.com>
2023-02-14 16:58:39 +0300

    realization "next" buttons for 1 and 2 screens

--- app/src/main/java/com/example/moskovchuk_lesson2/SecondActivity.kt
+++ app/src/main/java/com/example/moskovchuk_lesson2/SecondActivity.kt
@@ -1,11 +1,19 @@
 package com.example.moskovchuk_lesson2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val buttonNext2 : Button = findViewById(R.id.button_next2)
+        buttonNext2.setOnClickListener{
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
