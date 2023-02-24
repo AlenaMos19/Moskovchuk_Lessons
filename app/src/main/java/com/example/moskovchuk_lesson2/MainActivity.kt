85dd1bd17c91e3021f5299b9d6e51ee0781b7fbf
Alena Moskovchuk <am79819712119@gmail.com>
2023-02-24 17:53:24 +0300

    try created new navig bar

--- app/src/main/java/com/example/moskovchuk_lesson2/MainActivity.kt
+++ app/src/main/java/com/example/moskovchuk_lesson2/MainActivity.kt
@@ -2,26 +2,35 @@ package com.example.moskovchuk_lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.moskovchuk_lesson2.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
     //    supportFragmentManager.beginTransaction()
      //       .replace(R.id.container, AuthorizationFragment.newInstance())
      //       .commit()
 
-        navController = Navigation.findNavController(this, R.id.navigation_container)
-        setupWithNavController(binding.navView, navController)
+        val navController = findNavController(R.id.navigation_container)
+        val appBarConfig = AppBarConfiguration(setOf(R.id.bottom_main,
+        R.id.bottom_offices, R.id.bottom_vacancies))
+        setupActionBarWithNavController(navController, appBarConfig)
+        navView.setupWithNavController(navController)
     }
+
 }
