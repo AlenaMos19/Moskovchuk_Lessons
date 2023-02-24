d832e04af4a76ccbf63d00c6dc95573749b9ec5b
Alena Moskovchuk <am79819712119@gmail.com>
2023-02-24 11:52:33 +0300

    add navigation menu

--- app/src/main/java/com/example/moskovchuk_lesson2/MainActivity.kt
+++ app/src/main/java/com/example/moskovchuk_lesson2/MainActivity.kt
@@ -2,14 +2,26 @@ package com.example.moskovchuk_lesson2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.moskovchuk_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, AuthorizationFragment.newInstance())
-            .commit()
+    //    supportFragmentManager.beginTransaction()
+     //       .replace(R.id.container, AuthorizationFragment.newInstance())
+     //       .commit()
+
+        navController = Navigation.findNavController(this, R.id.navigation_container)
+        setupWithNavController(binding.navView, navController)
     }
 }
---
+++ app/src/main/java/com/example/moskovchuk_lesson2/OfficesFragment.kt
@@ -0,0 +1,19 @@
+package com.example.moskovchuk_lesson2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class OfficesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_offices, container, false)
+    }
+
+}
